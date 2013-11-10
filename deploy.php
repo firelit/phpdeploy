71880d5c09cf39abda5372aeed260ec6dc2cf557
Eric Bigoness <eric.bigoness@firelit.com>
2013-11-09 22:17:26 -0500

    Simplified uses

--- deploy.php
+++ deploy.php
@@ -4,12 +4,8 @@
 require_once('vendor/autoload.php');
 
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
 
 $application = new Application();
-$application->add(new DeployCommand);
-$application->add(new RollbackCommand);
+$application->add( new DeployCommand );
+$application->add( new RollbackCommand );
 $application->run();
